6e8c5c42b6e2bc1aab646d075c708f0bcb41f1be
“javascriptseo” <seofreelance2021@outlook.com>
2023-10-04 17:37:07 -0700

    complete

--- main.py
+++ main.py
@@ -9,7 +9,15 @@ head1 = "Number: "
 head2 = "Multiplied by 2: "
 head3 = "Multiplied by 10:  "
 NUM_LOOPS = 10  # Constant used to control loop.
+number = 0
 
 print("0 through 10 multiplied by 2 and by 10" + "\n")
 
 # Write while loop
+while True:
+    print (head1 + str(number))
+    print (head2 + str(number * 2))
+    print (head3 + str(number * 10))
+    number = number + 1
+    if number > 10:
+        break
